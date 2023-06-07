5a5d1b572a55df930270f6d10becff56ea2fde99
Bernard Calma <nard.calma@gmail.com>
2023-06-06 19:42:18 -0700

    chore(Promise): Change do first to be promise
    Add a promise object inside dofirst and move second innerHTML edit inside then

--- script/index.js
+++ script/index.js
@@ -23,21 +23,25 @@ let el = document.querySelector("#response")
 // When creating a promise you should add a function inside the promise paramenter always
 // resovle = success
 // reject = fail
-let myPromise = new Promise((resovle, reject)=>{
-    // resovle("Done!");
-    reject("Error");
+// let myPromise = new Promise((resovle, reject)=>{
+//     // resovle("Done!");
+//     reject("Error");
+// })
+
+// myPromise
+//     .then(res => console.log(res))
+//     .catch(err => console.log(err))
+
+const doFirst = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve("<br/> I was first...."), 2000);
+    });
+};
+
+doFirst()
+.then(data => {
+    el.innerHTML += data;
+    el.innerHTML += "<br/>And I am second!!!"
 })
+.catch(err => console.log(err));
 
-myPromise
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-    
-// const doFirst = () => {
-//     setTimeout(() => {
-//         el.innerHTML = el.innerHTML + "<br/> I was first...."
-//     }, 2000);
-// }
-
-// doFirst()
-
-// el.innerHTML = el.innerHTML + "<br/>And I am second!!!"
