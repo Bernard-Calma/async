c8c20e6c93984209b9fd3a14804c448c06ce2141
Bernard Calma <nard.calma@gmail.com>
2023-06-06 18:44:44 -0700

    chore(callback): Add c inside b to be a callback
    Add c as a parameter of b to be still alphabetically even with timeout

--- script/index.js
+++ script/index.js
@@ -1,11 +1,20 @@
 let el = document.querySelector("#response")
 
-const a = () => console.log("A");
+const a = () => {
+    console.log("A");
+};
 
-const b = (cb) => console.log("B");
+const b = (cb) => {
+    // A > C > B (after 2 seconds)
+        setTimeout(()=>{
+            console.log("B");
+            cb(); // Pass c inside b(c) to be A > (2 secs) > B > C
+        }, 2000)
+    //
+};
 
 const c = () => console.log("C");
 
 a();
-b();
-c()
+b(c);
+// c()
